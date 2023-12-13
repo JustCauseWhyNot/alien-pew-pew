1c00e81baa1c89600b3f977802f39b4f65f19642
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2023-12-13 11:05:36 -0700

    feat: add level display.

--- alien_invasion.py
+++ alien_invasion.py
@@ -59,6 +59,7 @@ class AlienInvasion:
 			for aliens in collisions.values():
 				self.stats.score += self.settings.alien_points * len(aliens)
 			self.sb.prep_score()
+			self.sb.prep_level()
 			self.sb.check_high_score()
 		if not self.aliens:
 			self.bullets.empty()
--- scoreboard.py
+++ scoreboard.py
@@ -14,6 +14,7 @@ class Scoreboard:
 		self.font = pygame.font.SysFont(None, 48)
 		self.prep_score()
 		self.prep_high_score()
+		self.prep_level()
 
 	def check_high_score(self):
 		"""check if high score"""
@@ -31,6 +32,14 @@ class Scoreboard:
 		self.high_score_rect.centerx = self.screen_rect.centerx
 		self.high_score_rect.top = self.score_rect.top
 
+	def prep_level(self):
+		"""Make level rendered"""
+		level_str = str(self.stats.level)
+		self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+		self.level_rect = self.level_image.get_rect()
+		self.level_rect.right = self.score_rect.right
+		self.level_rect.top = self.score_rect.bottom + 10
+
 	def prep_score(self):
 		"""Render score"""
 		rounded_score = round(self.stats.score, -1)
@@ -46,3 +55,4 @@ class Scoreboard:
 		"""Draw score"""
 		self.screen.blit(self.score_image, self.score_rect)
 		self.screen.blit(self.high_score_image, self.high_score_rect)
+		self.screen.blit(self.level_image, self.level_rect)
